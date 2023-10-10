df0a1561a2099e4ee52dd2ac01a3cb58d65f2aa6
Kodi Maberek <kodi.maberek23@gmail.com>
2023-10-09 22:29:23 -0400

    Ending build List Landmark

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Rodion Akhmedov on 10/9/23.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
