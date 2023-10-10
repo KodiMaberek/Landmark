90a5656065ef0ce1c254237384ad125d580a3a99
Kodi Maberek <kodi.maberek23@gmail.com>
2023-10-09 22:41:35 -0400

    End navigation Landmark List

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Rodion Akhmedov on 10/9/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
