99361e3a99db27b27d6dc838434b998eb3d2aaf8
Kodi Maberek <kodi.maberek23@gmail.com>
2023-10-09 21:49:15 -0400

    I created ContentView

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,11 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+                .foregroundColor(.primary)
+            HStack {
+                Text("Joshua Tree National Park")
+                
+                Spacer()
+                
+                Text("California")
+            }
+            .font(.subheadline)
+            .foregroundColor(.secondary)
+            Divider()
+            Text("About park")
+                .font(.title2)
+            Text("Descriptions goes here")
+                .font(.subheadline)
+                .foregroundColor(.secondary)
         }
         .padding()
     }
