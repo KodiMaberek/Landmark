771c2eddbb49db8c9eea8de840666eabe9477f5e
Kodi Maberek <kodi.maberek23@gmail.com>
2023-10-09 22:43:13 -0400

    Content View has LandmarkDetail

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,37 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        VStack {
-            MapView()
-                .frame(height: 300)
-                
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -150)
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                    .foregroundColor(.primary)
-                HStack {
-                    Text("Joshua Tree National Park")
-                    
-                    Spacer()
-                    
-                    Text("California")
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                Divider()
-                Text("About park")
-                    .font(.title2)
-                Text("Descriptions goes here")
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
-            }
-            .padding()
-        }
-        Spacer()
+      LandmarkDetail()
     }
 }
 
