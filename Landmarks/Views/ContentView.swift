61024d165b4829039d31d436878b99f0fc47d9b1
Kodi Maberek <kodi.maberek23@gmail.com>
2023-10-15 17:09:53 -0400

    updates home screen

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -7,11 +7,31 @@
 
 import SwiftUI
 
+import SwiftUI
+
+
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+
+
+    enum Tab {
+        case featured
+        case list
+    }
+
+
     var body: some View {
-      LandmarkList()
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem { Label("Futured", systemImage: "star") }
+
+
+            LandmarkList()
+                .tabItem { Label("List", systemImage: "list.bullet") }
+        }
     }
 }
+
 
 #Preview {
     ContentView()
