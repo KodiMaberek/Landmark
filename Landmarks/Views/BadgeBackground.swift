15bd0d35200dc0307a6bdad5dd496ed993d02f6b
Kodi Maberek <kodi.maberek23@gmail.com>
2023-10-15 12:40:47 -0400

    Fixed Landmark Detail

--- Landmarks/Views/BadgeBackground.swift
+++ Landmarks/Views/BadgeBackground.swift
@@ -5,13 +5,49 @@
 //  Created by Rodion Akhmedov on 10/14/23.
 //
 
+
 import SwiftUI
 
 struct BadgeBackground: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            Path { part in
+                var width: CGFloat = min(geometry.size.width, geometry.size.height)
+                let height = width
+                let xScale: CGFloat = 0.832
+                let xOffset = (width * (1.0 - xScale)) / 2.0
+                width *= xScale
+                part.move(
+                    to: CGPoint(  x: width * 0.95 + xOffset,
+                                  y: height * (0.20 + HexagonParameters.adjustment)
+                                  
+                               )
+                )
+                HexagonParameters.segment.forEach { segment in
+                    part.addLine(
+                        to: CGPoint(x: width * segment.line.x + xOffset,
+                                    y: height * segment.line.y))
+                    
+                    part.addQuadCurve(
+                        to: CGPoint(x: width * segment.curve.x + xOffset,
+                                    y: height * segment.curve.y),
+                        control: CGPoint(x: width * segment.control.x + xOffset,
+                                         y: height * segment.control.y))
+                }
+            }
+            .fill(.linearGradient(
+                Gradient(colors: [Self.gradientStart, Self.gradientEnd]),
+                startPoint: UnitPoint(x: 0.5, y: 0),
+                endPoint: UnitPoint(x: 0.5, y: 0.6)
+                )
+                  )
+        }
+        .aspectRatio(1, contentMode: .fit)
     }
+    static let gradientStart = Color(red: 239.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
+    static let gradientEnd = Color(red: 239.0 / 255, green: 172.0 / 255, blue: 120.0 / 255)
 }
+
 
 #Preview {
     BadgeBackground()
