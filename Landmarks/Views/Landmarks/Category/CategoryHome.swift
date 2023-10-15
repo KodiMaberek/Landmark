b1de9c118c2913d521ad2c44de421fd7220c64a2
Kodi Maberek <kodi.maberek23@gmail.com>
2023-10-15 17:06:17 -0400

    add category for Home screen

--- Landmarks/Views/Landmarks/Category/CategoryHome.swift
+++ Landmarks/Views/Landmarks/Category/CategoryHome.swift
@@ -8,21 +8,31 @@
 import SwiftUI
 
 struct CategoryHome: View {
-    @Environment(ModelData.self) var modelData
+    @Environment(ModelData.self)var modelData
     var body: some View {
         NavigationSplitView {
             List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
                 ForEach(modelData.category.keys.sorted(), id: \.self) { key in
-                    Text(key)}
+                    CategoryRow(categoryName: key, items: modelData.category[key]!)
+                }
+                .listRowInsets(EdgeInsets())
             }
-                .navigationTitle("Futured")
+            .navigationTitle("Futured")
         } detail: {
-            Text("Select a landmark")
+            Text("Select a Landmark")
         }
     }
+    
 }
 
 #Preview {
     CategoryHome()
         .environment(ModelData())
+       
 }
