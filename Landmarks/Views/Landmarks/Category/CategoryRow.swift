7e5bc1478dfa9009b3528393390d3be400b54a1c
Kodi Maberek <kodi.maberek23@gmail.com>
2023-10-15 17:06:46 -0400

    Category row

--- Landmarks/Views/Landmarks/Category/CategoryRow.swift
+++ Landmarks/Views/Landmarks/Category/CategoryRow.swift
@@ -10,20 +10,24 @@ import SwiftUI
 struct CategoryRow: View {
     var categoryName: String
     var items: [Landmark]
-    
     var body: some View {
         VStack(alignment: .leading) {
-            Text(categoryName)
+        Text(categoryName)
                 .font(.headline)
                 .padding(.leading, 15)
                 .padding(.top, 5)
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(alignment: .top, spacing: 0) {
                     ForEach(items) { landmark in
-                        CategoryItem(landmark: landmark)
+                        NavigationLink {
+                            LandmarkDetail(landmark: landmark)
+                        } label: {
+                            CategoryItem(landmark: landmark)
+                        }
+             
                     }
+                    .frame(width: 185)
                 }
-                .frame(height: 185)
             }
         }
     }
@@ -31,5 +35,7 @@ struct CategoryRow: View {
 
 #Preview {
     let landmarks = ModelData().landmarks
-    return CategoryRow(categoryName: landmarks[0].category.rawValue, items: Array(landmarks.prefix(3)))
+    return CategoryRow(
+        categoryName: landmarks[0].category.rawValue,
+        items: Array(landmarks.prefix(3)))
 }
