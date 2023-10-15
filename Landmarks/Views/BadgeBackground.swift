f924142244272d8ab9682220b152705ce66855ad
Kodi Maberek <kodi.maberek23@gmail.com>
2023-10-15 12:39:17 -0400

    hexagon parameters

---
+++ Landmarks/Views/BadgeBackground.swift
@@ -0,0 +1,18 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by Rodion Akhmedov on 10/14/23.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeBackground()
+}
